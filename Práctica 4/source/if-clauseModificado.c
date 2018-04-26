3ab7205e7baa249ce3727b91d3152d8dfb54cf06
antoniogamiz <antoniogamiz10@gmail.com>
2018-04-26 20:55:37 +0200

    Ejercicio 1 done danone

---
+++ Práctica 4/source/if-clauseModificado.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(int argc, char **argv)
+{
+    int i, n=20, tid, x;
+    int a[n], suma=0, sumalocal;
+
+    if( argc != 3 )
+    {
+        fprintf(stderr, "[Formato] ./if-clause <iteraciones> <n-threads> \n]");
+        exit(-1);
+    }
+
+    n=atoi(argv[1]); if( n > 20 ) n=20;
+    x=atoi(argv[2]); if( x > 8 ) n=8; //Adaptado a mi pc
+    for(i=0; i<n; i++) a[i]=i;
+
+    #pragma omp parallel if(n>4) default(none) private(sumalocal, tid) \
+            shared(a, suma, n) num_threads(x)
+    {
+        sumalocal=0;
+        tid=omp_get_thread_num();
+        #pragma omp for private(i) schedule(static) nowait
+        for(i=0; i<n; i++)
+        {
+            sumalocal+=a[i];
+            printf("thread %d suma de a[%d]=%d sumalocal=%d\n", tid, i, a[i], sumalocal);
+        }
+    #pragma omp atomic
+        suma+=sumalocal;
+    #pragma omp barrier
+    #pragma omp master
+        printf("thread master=%d imprime suma=%d\n", tid, suma);
+    }
+
+}
